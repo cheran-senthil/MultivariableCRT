6151b971b1b1dd1a0672b2eda0fd18b1c058edb9
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-07-23 17:38:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
+"""Multivariable Chinese Remainder Theorem"""
+
+from math import gcd
 import numpy as np
 
-def MCRT(A, b, m):
-    pass
+def egcd(a, m):
+    """Extended GCD"""
+    if a == 0:
+        return (m, 0, 1)
+    g, y, x = egcd(m % a, a)
+    return (g, x - (m // a) * y, y)
+
+def modinv(a, m):
+    """Find Modular Inverse"""
+    amodm = a % m
+    g, x, _ = egcd(amodm, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+def pivot(A, m):
+    """Finds the pivot of A and m"""
+    length = len(A)
+    result = [0]*length
+    for i in range(length):
+        for j in range(length):
+            if gcd(A[i][j], m[i]) == 1:
+                result[i] = j
+    return result
 
 def is_sol(A, x, b, m):
-    pass
+    """Checks if Ax = b mod m"""
+    ax_b = np.matmul(np.array(A), np.array(x)) - np.array(b)
+    for i, mod in enumerate(m):
+        if ax_b[i] % mod != 0:
+            return False
+    return True
+
+def mcrt(A, b, m):
+    """Returns for x in Ax = b mod m"""
+    eqn_cnt = len(A)
+    piv = pivot(A, m)
+    x = [0]*eqn_cnt
+    m_prod = 1
+
+    for i in range(eqn_cnt):
+        tot = sum(A[i][k] * x[k] for k in range(eqn_cnt))
+        tmp = (modinv(m_prod * A[i][piv[i]], m[i]) * (b[i] - tot)) % m[i]
+        x[piv[i]] = x[piv[i]] + tmp * m_prod
+        m_prod *= m[i]
+
+    return x
